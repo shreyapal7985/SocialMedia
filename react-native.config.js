929d7ce3bc0a5904c47fbb4d11aecf837573a2c6
Shreya pal <shreyapal7985@gmail.com>
2026-07-02 22:14:00 +0530

    config file has created to tell cli where custom files or assets are located

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,9 @@
+/*assets/fonts = your house
+react-native.config.js = the address note
+npx react-native-asset = the delivery person
+
+Without the note, the delivery person doesn't know where to go. */
+
 module.exports={
     project:{
         ios:{},
