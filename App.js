6455005574cac23a85ecd9f68a63044c7b527f5a
Shreya pal <shreyapal7985@gmail.com>
2026-07-05 23:52:52 +0530

    styling

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 /*fontWeight is not working on android when we are applying fontFamily
 Ways of applying fontFamily ---1fontFamily:'Inter_18pt-Black',  */
-import {Text,View} from 'react-native';
+import {Text,TouchableOpacity,View} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {getFontFamily} from './assets/fonts/helper';
-import Title from './component/Title/title.js'
+import Title from './component/Title/title.js';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const App=()=>{
   return(
     <SafeAreaView>
+    <View style={{marginLeft:30, 
+    marginRight:17, 
+    marginTop:30, 
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:"space-between"}}>
     <Title title={'Let’s Explore '}/>
+    <TouchableOpacity style={{backgroundColor:'#f5f7faf4', padding:14, borderRadius:100}}>
+    <FontAwesomeIcon icon={faEnvelope} color={'#898DAE'}/>
+    </TouchableOpacity>
+    </View>
     </SafeAreaView>
 
   )
