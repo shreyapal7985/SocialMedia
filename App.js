2b9914a7a546ffd1146cad9edef1f738d6d5b05a
Shreya pal <shreyapal7985@gmail.com>
2026-07-16 09:48:17 +0530

    user posts list

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import globalstyle from './assets/styles/globalstyle';
 import style from './assets/styles/globalstyle';
 
 import UserStory from './component/UserStory/UserStory';
+import { useEffect, useState } from 'react';
 
 const App = () => {
   const stories = [
@@ -59,6 +60,87 @@ const App = () => {
       profile: require('./assets/images/default_profile.png')
     },
   ]
+  const posts=[
+    {
+      firstName:'adam',
+      lastName:'sharma',
+      location:'boston, Ma',
+      likes:1202,
+      comments:120,
+      bookmark:55,
+      id:1
+    },
+    {
+      firstName:'mala',
+      lastName:'kali',
+      location:'barely, ind',
+      likes:120,
+      comments:10,
+      bookmark:5,
+      id:2
+    },
+    {
+      firstName:'advin',
+      lastName:'singh',
+      location:'cicago, Ma',
+      likes:1000,
+      comments:20,
+      bookmark:65,
+      id:3
+    },
+    {
+      firstName:'damini',
+      lastName:'mittal',
+      location:'agra,ind',
+      likes:202,
+      comments:1120,
+      bookmark:505,
+      id:4
+    },
+    {
+      firstName:'ladam',
+      lastName:'sharma',
+      location:'boston, Ma',
+      likes:1000,
+      comments:20,
+      bookmark:55,
+      id:5
+    }
+  ]
+
+
+//All for  stories infinite scrolling
+  const userStoryPageSize=4;
+  const [userStoriesCurrentPage, setUserStoriesCurrentPage]=useState(1);
+  const [userStoriesRenderData, setUserStoriesRenderData]=useState([]);
+  const [isLoadingUserStories,setIsLoadingUserStories]=useState(false)
+
+  //All for posts infinite scrolling
+  const userPostsPageSize=4;
+  const [userPostsCurrentPage, setUserCurrentPostsPage]=useState(1);
+  const [userPostsRenderData, setUserPostsRenderData]=useState([]);
+  const [isLoadingUserPosts,setIsLoadingUserPosts]=useState(false)
+
+//Pagination for stories 
+  const pagination=(database, currentPage, pageSize)=>{
+    console.log(currentPage)
+    const startIndex =(currentPage-1)*pageSize;
+    const endIndex= startIndex+pageSize;
+    if (startIndex>=database.length)
+    {
+      return[];
+  }
+  return database.slice(startIndex, endIndex);
+}
+
+//handle the side effect of stories
+  useEffect(()=>{
+    setIsLoadingUserStories(true)
+    const getInitialData=pagination(stories,1,userStoryPageSize)
+    setUserStoriesRenderData(getInitialData)
+    setIsLoadingUserStories(false)
+  },[])
+  
   return (
     <SafeAreaView>
 
@@ -77,10 +159,26 @@ const App = () => {
       //USER STORY SECTION BEGIN
 <View style={style.userStoryContainer}>
 <FlatList 
+onEndReachedThreshold={0.5}
+onEndReached={()=>{
+  if(isLoadingUserStories)
+  {
+    return;
+  }
+  setIsLoadingUserStories(true);
+  const contentToAppend=pagination(stories,userStoriesCurrentPage+1,userStoryPageSize)
+  if(contentToAppend.length>0){
+    setUserStoriesCurrentPage(userStoriesCurrentPage+1);
+setUserStoriesRenderData(prev=>[...prev,...contentToAppend])
+  }
+  setIsLoadingUserStories(false)
+}}
 showsHorizontalScrollIndicator={false}
 horizontal={true} 
-  data={stories}
-  renderItem={({item})=><UserStory firstName={item.firstName} profile={item.profile}/>}
+  data={userStoriesRenderData}
+  renderItem={({item})=><UserStory 
+  key={item.id}
+  firstName={item.firstName} profile={item.profile}/>}
 />
 </View>
 
